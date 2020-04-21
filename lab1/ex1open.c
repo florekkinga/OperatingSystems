e552932dffcaa92e43577a68e06f643cced233b9
florekkinga <florek.kinga@gmail.com>
2020-04-21 15:02:02 +0200

    update ex1open.c

--- lab1/ex1open.c
+++ lab1/ex1open.c
@@ -4,18 +4,16 @@
 #include <unistd.h> //read function itd.
 #include <errno.h> //variable errno
 #include <string.h> //strerror
-#include "t.h" //timestart, timestop
  
 #define BUFSIZE 1024
  
 int main (int argc, char **argv) {
-    int fp, chars; //fp - file pointer, 
-    char b[BUFSIZE], *fname;
+    int fp, chars; //fp - file pointer - deskryptor 
+    char b[BUFSIZE], *fname; //bufor b jest tablicą znaków o rozmiarze BUFFSIZE=1024
  
     chars = 10;
     fname = argv[1];
- 	
-    timestart();
+ 
     if((fp = open (fname, O_RDONLY)) == -1){
             printf("Oh dear, something went wrong with open()! %s\n", strerror(errno));
             exit(EXIT_FAILURE);
@@ -24,10 +22,8 @@ int main (int argc, char **argv) {
             printf("Oh dear, something went wrong with open()! %s\n", strerror(errno));
             exit(EXIT_FAILURE);
     }
-    timestop("Execution Time");
 
-    printf("%s: Przeczytano %d znakow z pliku %s: \"%s\"\n",
-	   argv[0], chars, fname, b);
+    printf("%s: Przeczytano %d znakow z pliku %s: \"%s\"\n", argv[0], chars, fname, b);
     close(fp);
  
     return(0);
